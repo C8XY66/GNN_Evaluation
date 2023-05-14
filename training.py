a9181a83a2a884cf39315be3972ef624e0cac6f9
johanna <studer.johanna@gmail.com>
2023-05-14 16:47:47 +0200

    hyperparameter defaults, gin_train_eps, dgcnn_k

--- training.py
+++ training.py
@@ -47,32 +47,42 @@ def create_trainer(log_dir, epochs, patience=None, pruning_callback=None, testin
     return trainer
 
 
-def objective(trial, datamodule, log_dir, epochs, patience, model_name, dataset_type):
+def objective(trial, datamodule, log_dir, epochs, patience, model_name, dataset_type, dgcnn_k):
     config_file = f"config_{model_name}.yaml"
     config = load_config(config_file=config_file)
 
-    # Optimise hyperparameters
-    hyperparameters = {}
+    # OPTIMISE HYPERPARAMETERS
+    # Set default values
+    hyperparameters = {"num_layers": 0,
+                       "hidden_channels": 0,
+                       "batch_size": 0,
+                       "learning_rate": 0.0,
+                       "dropout": 0.0,
+                       "weight_decay": 0.0,
+                       "gin_train_eps": False,
+                       "dgcnn_k": 0}
 
+    # Check config file for values, and choose hyperparameter combination
     for param, values in config.items():
         if isinstance(values, list):
             hyperparameters[param] = trial.suggest_categorical(param, values)
         else:
             hyperparameters[param] = values
 
-    # Model and DataModule
-    datamodule.update_batch_size(hyperparameters["batch_size"])
-    model = GNNModel(gnn_model_name=model_name,
-                     in_channels=datamodule.num_node_features,
-                     out_channels=datamodule.num_classes,
-                     hidden_channels=hyperparameters["hidden_channels"],
-                     num_layers=hyperparameters["num_layers"],
-                     dropout=hyperparameters["dropout"],
-                     learning_rate=hyperparameters["learning_rate"],
-                     weight_decay=hyperparameters["weight_decay"],
-                     dataset_type=dataset_type)
-
-    # Training
+        # Pass hyperparameters to Model and DataModule
+        datamodule.update_batch_size(hyperparameters["batch_size"])
+        model = GNNModel(gnn_model_name=model_name,
+                         dataset_type=dataset_type,
+                         in_channels=datamodule.num_node_features,
+                         out_channels=datamodule.num_classes,
+                         hidden_channels=hyperparameters["hidden_channels"],
+                         num_layers=hyperparameters["num_layers"],
+                         learning_rate=hyperparameters["learning_rate"],
+                         dropout=hyperparameters["dropout"],
+                         weight_decay=hyperparameters["weight_decay"],
+                         gin_train_eps=hyperparameters["gin_train_eps"],
+                         dgcnn_k=dgcnn_k)
+    # TRAINING
     pruning_callback = PyTorchLightningPruningCallback(trial=trial, monitor="val_acc")
     trainer = create_trainer(log_dir=log_dir, epochs=epochs, patience=patience,
                              pruning_callback=pruning_callback, trial=trial)
