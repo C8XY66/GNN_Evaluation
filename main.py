559e727ddc7c12047fdcd8e6f7eb508680d0d83f
johanna <studer.johanna@gmail.com>
2023-05-28 12:47:04 +0200

    Replaced COLLAB with IMDB-MULTI

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 # USER INPUTS
 parser = argparse.ArgumentParser()
+
+parser.add_argument("--MAIN_DIR", type=str, default="/Users/johanna/PycharmProjects/",
+                    help="main directory of project, where logs are saved")
+
 parser.add_argument("--EXPERIMENT", type=str, default='WithNF',
                     help="type of experiment: WithNF, WithoutNF (default: WithNF). "
                          "For graphs with or without node features")
 parser.add_argument("--MODEL", type=str, default="GIN", help="name of model: GIN, DGCNN, MLP (default: GIN)")
 parser.add_argument("--DATASET", type=str, default="MUTAG", help="name of dataset: MUTAG, NCI1, PROTEINS, IMDB-BINARY, "
-                                                                 "COLLAB (default: MUTAG)")
+                                                                 "IMDB-MULTI (default: MUTAG)")
 parser.add_argument("--FOLDS", type=int, default=10, help="number of folds dataset is split into")
 parser.add_argument("--REPS", type=int, default=10, help="number of total repetitions")
 parser.add_argument("--EPOCHS", type=int, default=1000, help="number of epochs to train each trial of fold")
@@ -35,11 +39,8 @@ parser.add_argument("--STOP_REP", type=int, default=None, help="at which repetit
 parser.add_argument("--START_FOLD", type=int, default=0, help="from which fold to start (default: 0)")
 parser.add_argument("--STOP_FOLD", type=int, default=None, help="at which fold to stop (default: None)")
 
-parser.add_argument("--MAIN_DIR", type=str, default="/Users/johanna/PycharmProjects/",
-                    help="main directory of project, where logs are saved")
 parser.add_argument("--PARENT_DIR", type=str, default=None,
-                    help="name of parent directory for resuming interrupted run (default: None). Use format like "
-                         "'/Users/johanna/PycharmProjects/logs/GIN_NCI1_reps_2_folds_5_epochs_100_2023-04-26_09-00'")
+                    help="name of parent directory for resuming interrupted run (default: None).")
 parser.add_argument("--RUNNING_MODE", type=str, default="single",
                     help="running mode: single, parallel (default: 'single'). Code executed as single run or folds run "
                          "separately in parallel")
@@ -50,11 +51,11 @@ if args.EXPERIMENT not in ["WithNF", "WithoutNF"]:
     raise ValueError("Experiment must be 'WithNF' or 'WithoutNF'")
 if args.MODEL not in ["GIN", "DGCNN", "MLP"]:
     raise ValueError("Model name must be 'GIN' or 'DGCNN'")
-if args.DATASET not in ["MUTAG", "NCI1", "PROTEINS", "COLLAB", "IMDB-BINARY"]:
-    raise ValueError("Dataset name must be one of the following: 'MUTAG', 'NCI1', 'PROTEINS', 'COLLAB', 'IMDB-BINARY'")
+if args.DATASET not in ["MUTAG", "NCI1", "PROTEINS", "IMDB-BINARY", "IMDB-MULTI"]:
+    raise ValueError("Dataset name must be one of the following: 'MUTAG', 'NCI1', 'PROTEINS', 'IMDB-BINARY', "
+                     "'IMDB-MULTI'")
 if args.PARENT_DIR is not None and not os.path.isdir(args.PARENT_DIR):
-    raise ValueError("Invalid directory, should be of format: "
-                     "'/Users/johanna/PycharmProjects/logs/GIN_NCI1_reps_2_folds_5_epochs_100_2023-04-26_09-00' ")
+    raise ValueError("Invalid directory")
 if args.RUNNING_MODE not in ["single", "parallel"]:
     raise ValueError("Running mode must be one of the following: 'single', 'parallel'")
 
@@ -66,7 +67,7 @@ if __name__ == "__main__":
 
     # Dataset type passed to DataModule (data preprocessing) and objective -> model (type of MLP baseline)
     dataset_type = "chemical" if args.DATASET in ["MUTAG", "NCI1", "PROTEINS"] \
-        else "social" if args.DATASET in ["COLLAB", "IMDB-BINARY"] else None
+        else "social" if args.DATASET in ["IMDB-BINARY", "IMDB-MULTI"] else None
 
     # Create log folder
     parent_dir_info = f"{args.MODEL}_{args.DATASET}_{args.EXPERIMENT}_reps_{args.REPS}_folds_{args.FOLDS}" \
